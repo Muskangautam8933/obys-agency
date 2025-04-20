024cb5953635f319630ba82891f3ac040949dca0
Muskan Gautam <muskangautam7064@gmail.com>
2025-04-21 00:28:13 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline();
+function loadingAnimation(){
+  var tl = gsap.timeline();
 tl.from(".line h1", {
   y: 150,
   stagger: 0.25,
@@ -43,28 +44,24 @@ tl.to("#loader",{
   display:"none",
   duration:0.5
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+tl.from("nav",{
+  opacity:0
+})
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2 , #hero4 h1",{
+   y:150,
+   stagger:0.2,
+})
+}
+function cursorAnimation(){
+  //cursor animation
+document.addEventListener("mousemove",function(dets){
+  gsap.to("#crsr",{
+    left:dets.x,
+    top:dets.y
+  })
+})
+//shery javascript
+Shery.makeMagnet(".nav-part2 h4");
+}
+loadingAnimation();
+cursorAnimation();
